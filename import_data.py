6966cc7f252901af2b7ae22dabb418466baca25a
Kevin Traver <kevintraver@kevintraver.com>
2023-08-18 19:29:55 -0600

    update data import script

--- import_data.py
+++ import_data.py
@@ -1,13 +1,24 @@
 import json
 import sqlite3
 
-def import_landmarks_from_json(json_data, db_path):
+def import_landmarks_and_category_from_json(json_data, db_path):
     # Connect to the SQLite database
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
 
     # Extract the category name and landmarks from the json_data
     category_name, landmarks = list(json_data.items())[0]
+
+    # Check if the category already exists in the categories table
+    cur.execute("SELECT category_id FROM categories WHERE name = ?", (category_name,))
+    category_id = cur.fetchone()
+
+    # If the category doesn't exist, insert it
+    if not category_id:
+        cur.execute("INSERT INTO categories (name) VALUES (?)", (category_name,))
+        category_id = cur.lastrowid
+    else:
+        category_id = category_id[0]
 
     # Loop through the landmarks and insert them into the database
     for landmark in landmarks:
@@ -23,14 +34,9 @@ def import_landmarks_from_json(json_data, db_path):
 
         # Retrieve the ID of the inserted landmark
         landmark_id = cur.lastrowid
-
-        # Get the category_id for the category_name from the categories table
-        cur.execute("SELECT category_id FROM categories WHERE name = ?", (category_name,))
-        category_id = cur.fetchone()
-        if category_id:
-            category_id = category_id[0]
-            # Insert the association into the landmarks_categories table
-            cur.execute("INSERT INTO landmarks_categories (landmark_id, category_id) VALUES (?, ?)", (landmark_id, category_id))
+        
+        # Insert the association into the landmarks_categories table
+        cur.execute("INSERT INTO landmarks_categories (landmark_id, category_id) VALUES (?, ?)", (landmark_id, category_id))
 
     # Commit the changes and close the cursor and connection
     conn.commit()
@@ -39,9 +45,7 @@ def import_landmarks_from_json(json_data, db_path):
 
     return "Data imported successfully!"
 
-# Load data from the JSON file
-with open('landmarks.json', 'r') as file:
-    data = json.load(file)
-
-# Run the import function
-import_landmarks_from_json(data, 'landmarks.db')
+# To use the script, load your JSON data and call the function:
+# with open('landmarks.json', 'r') as file:
+#     data = json.load(file)
+# import_landmarks_and_category_from_json(data, 'landmarks.db')
